d5d297b4bcd8db3c2a7e87749884af0b8d708585
Prat <banqfirn@gmail.com>
2015-02-16 18:12:25 -0500

    Add application class

---
+++ app/src/main/java/com/prat/gregeek/GreApp.java
@@ -0,0 +1,13 @@
+package com.prat.gregeek;
+
+import android.app.Application;
+
+/**
+ * Created by pt2121 on 2/16/15.
+ */
+public class GreApp extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}

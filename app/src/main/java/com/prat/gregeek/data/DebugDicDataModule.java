498076a25efab3292bf6303df0fc36d3206a0d12
Prat <banqfirn@gmail.com>
2015-02-16 21:41:21 -0500

    Add mockito

--- app/src/main/java/com/prat/gregeek/data/DebugDicDataModule.java
+++ app/src/main/java/com/prat/gregeek/data/DebugDicDataModule.java
@@ -1,8 +1,29 @@
 package com.prat.gregeek.data;
 
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Created by pt2121 on 2/16/15.
  */
+@Module
 public class DebugDicDataModule {
+    private final boolean mockMode;
 
+    public DebugDicDataModule(boolean provideMocks) {
+        mockMode = provideMocks;
+    }
+
+    @Provides
+    @Singleton
+    DicData provideDicDataModule() {
+        return new DicData();
+//        if (mockMode) {
+//            return Mockito.mock(Api.class);
+//        } else {
+//            return new DicData();
+//        }
+    }
 }
